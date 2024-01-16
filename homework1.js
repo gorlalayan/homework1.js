558d90e9bf214774e2935f7392843cd5382b43c5
gorlalayan <154901579+gorlalayan@users.noreply.github.com>
2024-01-16 13:45:33 +0400

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,43 @@
+//homework1
+// function shallowCompare(obj1, obj2) {
+//     if (typeof obj1 === "object" || typeof obj2 === "object") {
+//         return true
+//     }
+//     if (obj1.lenght === obj2.lenght) {
+//         return true
+//     }
+//     for (let key in obj1) {
+//         if (obj1[key] === obj2[key]) {
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+
+
+
+//homework4
+// function countMissingNumbers(arr) {
+//     if (arr.length <= 1) {
+//         return 0; 
+//     }
+//     const min = Math.min(...arr);
+//     const max = Math.max(...arr);
+//     const calculate = max - min + 1;
+//     const missingCount = calculate - arr.length;
+//     return missingCount;
+// }
+
+
+
+//homework5
+// function elementExists(arr, element) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === element) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
